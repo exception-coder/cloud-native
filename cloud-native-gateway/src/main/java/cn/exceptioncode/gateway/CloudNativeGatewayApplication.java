12c71fd3f9539e8bfc4556a3a1a03da86f951ecc
zhangkai <425485346@qq.com>
2019-05-20 18:21:13 +0800

    网关服务

---
+++ cloud-native-gateway/src/main/java/cn/exceptioncode/gateway/CloudNativeGatewayApplication.java
@@ -0,0 +1,13 @@
+package cn.exceptioncode.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudNativeGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudNativeGatewayApplication.class, args);
+    }
+
+}
