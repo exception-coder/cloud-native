c907ceb1f63e38081fe84415ef33003e5a284f47
zhangkai <425485346@qq.com>
2019-05-23 22:23:34 +0800

    网关服务

--- cloud-native-web/src/main/java/cn/exceptioncode/webapp/controller/WebController.java
+++ cloud-native-web/src/main/java/cn/exceptioncode/webapp/controller/WebController.java
@@ -1,9 +1,11 @@
 package cn.exceptioncode.webapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+@RequestMapping("/foo")
 @RestController
 public class WebController {
     @GetMapping("/index")
