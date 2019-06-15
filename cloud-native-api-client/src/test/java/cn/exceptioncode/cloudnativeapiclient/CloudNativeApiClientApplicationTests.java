9c04efb8f8011b3234e5fae4fa5997ea9d944e15
张凯 <425485346@qq.com>
2019-06-15 17:11:31 +0800

    Yapi接口测试

--- cloud-native-api-client/src/test/java/cn/exceptioncode/cloudnativeapiclient/CloudNativeApiClientApplicationTests.java
+++ cloud-native-api-client/src/test/java/cn/exceptioncode/cloudnativeapiclient/CloudNativeApiClientApplicationTests.java
@@ -1,6 +1,8 @@
 package cn.exceptioncode.cloudnativeapiclient;
 
 import cn.exceptioncode.api.doc.client.CloudNativeApiClientApplication;
+import cn.exceptioncode.api.doc.client.dto.AddApiDTO;
+import cn.exceptioncode.api.doc.client.feign.YapiClient;
 import cn.exceptioncode.api.doc.client.service.YapiService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
@@ -14,13 +16,25 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest(classes = CloudNativeApiClientApplication.class,webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CloudNativeApiClientApplicationTests {
 
+	@Autowired
+	YapiClient yapiClient;
 
 	@Autowired
 	YapiService yapiService;
 
 	@Test
 	public void contextLoads() {
-		System.out.println(yapiService.loginCookie());
+		AddApiDTO addApiDTO = new AddApiDTO();
+		addApiDTO.setTitle("开发调试测试接口");
+		addApiDTO.setPath("/api/group/list");
+		addApiDTO.setDesc("接口描述。。。。 开发调试数据");
+		addApiDTO.setMethod("GET");
+		addApiDTO.setStatus("undone");
+		addApiDTO.setRes_body("hello world");
+		addApiDTO.setRes_body_type("raw");
+		addApiDTO.setSwitch_notice(true);
+		addApiDTO.setMessage("message");
+		System.out.println(yapiClient.interfaceSave(addApiDTO,yapiService.loginCookie()));
 	}
 
 }
