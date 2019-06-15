a51988e3c7aa0c1ce06a8a74bbfe1f9fedbef611
张凯 <425485346@qq.com>
2019-06-15 15:58:57 +0800

    Yapi接口测试

--- cloud-native-api-client/src/main/java/cn/exceptioncode/api/doc/client/service/YapiService.java
+++ cloud-native-api-client/src/main/java/cn/exceptioncode/api/doc/client/service/YapiService.java
@@ -1,7 +1,13 @@
 package cn.exceptioncode.api.doc.client.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import cn.exceptioncode.api.doc.client.feign.YapiClient;
+import feign.Response;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  * @author zhangkai
@@ -9,4 +15,34 @@ import org.springframework.stereotype.Service;
 @Service
 public class YapiService {
 
+    private final YapiClient yapiClient;
+
+    public YapiService(YapiClient yapiClient) {
+        this.yapiClient = yapiClient;
+    }
+
+    /**
+     *
+     * 获取 token 相关信息
+     *
+     * @return
+     */
+    public String loginCookie() {
+        Map<String, String> map = new HashMap<>(2);
+        map.put("email", "425485346@qq.com");
+        map.put("password", "ymfe.org");
+        Response response = yapiClient.userLogin(map);
+        Collection<String> list = response.headers().get("set-cookie");
+        String cookie = "";
+        Iterator<String> iterator = list.iterator();
+        boolean isFirst = true;
+        while (iterator.hasNext()) {
+            if (!isFirst) {
+                cookie += ";";
+            }
+            cookie += iterator.next();
+            isFirst = false;
+        }
+        return cookie;
+    }
 }
--- cloud-native-api-client/src/test/java/cn/exceptioncode/cloudnativeapiclient/CloudNativeApiClientApplicationTests.java
+++ cloud-native-api-client/src/test/java/cn/exceptioncode/cloudnativeapiclient/CloudNativeApiClientApplicationTests.java
@@ -1,8 +1,7 @@
 package cn.exceptioncode.cloudnativeapiclient;
 
 import cn.exceptioncode.api.doc.client.CloudNativeApiClientApplication;
-import cn.exceptioncode.api.doc.client.feign.YapiClient;
-import feign.Response;
+import cn.exceptioncode.api.doc.client.service.YapiService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,35 +9,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.*;
-
 @Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = CloudNativeApiClientApplication.class,webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CloudNativeApiClientApplicationTests {
 
+
 	@Autowired
-	YapiClient yapiClient;
+	YapiService yapiService;
 
 	@Test
 	public void contextLoads() {
-		Map<String,String> map = new HashMap<>(2);
-		map.put("email","425485346@qq.com");
-		map.put("password","ymfe.org");
-		Response response = yapiClient.userLogin(map);
-		Collection<String> list = response.headers().get("set-cookie");
-		String cookie = "";
-		Iterator<String> iterator = list.iterator();
-		boolean isFirst = true;
-		while (iterator.hasNext()){
-			if(!isFirst){
-				cookie+=";";
-			}
-			cookie+=iterator.next();
-			isFirst = false;
-		}
-		log.warn("Cookie:{}",cookie);
-
+		System.out.println(yapiService.loginCookie());
 	}
 
 }
