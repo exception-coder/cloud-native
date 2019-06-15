380478be12e14906cd4a2d51ea0f971628e42e26
张凯 <425485346@qq.com>
2019-06-15 16:18:20 +0800

    Yapi接口测试

--- cloud-native-api-client/src/main/java/cn/exceptioncode/api/doc/client/feign/YapiClient.java
+++ cloud-native-api-client/src/main/java/cn/exceptioncode/api/doc/client/feign/YapiClient.java
@@ -1,5 +1,6 @@
 package cn.exceptioncode.api.doc.client.feign;
 
+import cn.exceptioncode.api.doc.client.dto.AddApiDTO;
 import feign.Response;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,25 @@ import java.util.Map;
 @FeignClient(name = "yapi",url = "http://129.28.173.232:3000/api")
 public interface YapiClient {
 
+    /**
+     *
+     * 用户登录
+     *
+     * @param map
+     * @return
+     */
     @PostMapping("/user/login")
     Response userLogin(@RequestBody Map map);
+
+
+    /**
+     *
+     * 新增接口
+     *
+     * @return
+     */
+    @PostMapping("/interface/add")
+    String interfaceAdd(@RequestBody AddApiDTO addApiDTO);
+
+
 }
