7680eed285612ddcb24868cb3a86116bd0799e16
张凯 <425485346@qq.com>
2019-06-17 14:52:45 +0800

    日常补全修复更新

--- cloud-native-api-client/src/main/java/cn/exceptioncode/api/doc/client/feign/YapiClient.java
+++ cloud-native-api-client/src/main/java/cn/exceptioncode/api/doc/client/feign/YapiClient.java
@@ -3,6 +3,7 @@ package cn.exceptioncode.api.doc.client.feign;
 import cn.exceptioncode.api.doc.client.dto.AddApiDTO;
 import feign.Response;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -12,6 +13,7 @@ import java.util.Map;
 /**
  * @author zhangkai
  */
+@Service
 @FeignClient(name = "yapi",url = "http://129.28.173.232:3000/api")
 public interface YapiClient {
 
--- cloud-native-web/src/main/java/cn/exceptioncode/webapp/controller/WebController.java
+++ cloud-native-web/src/main/java/cn/exceptioncode/webapp/controller/WebController.java
@@ -7,6 +7,8 @@ import org.springframework.cloud.netflix.ribbon.RibbonClientConfiguration;
 import org.springframework.http.MediaType;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
@@ -24,6 +26,14 @@ public class WebController {
 
     @Autowired
     WebService webService;
+
+
+    @GetMapping("/test/javadoc/{foo}")
+    public Mono<String> javadoc(@RequestParam(name = "userName",required = false) String userName,
+                                @PathVariable(name = "foo") String foo){
+        return Mono.just("hello world javadoc,userName:"+userName+",foo:"+foo);
+    }
+
 
     @GetMapping("/exception")
     public void exception() throws Exception {
